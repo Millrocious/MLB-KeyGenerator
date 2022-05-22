0400515113d15e8742c4ebc80bba548e714d885b
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2022-05-22 14:17:24 +0300

    Update

--- Controllers/KeysFinder.java
+++ Controllers/KeysFinder.java
@@ -1,11 +1,14 @@
 package Controllers;
 
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 public class KeysFinder {
     int base, power;
     HashMap<KeysGenerator, Boolean> keysGenerators;
     ArrayList<int[]> listOfKeys;
+    List<int[]> results = new ArrayList<>();
 
     public KeysFinder(int base, int power, HashMap<KeysGenerator, Boolean> keysGenerators) {
         this.base = base;
@@ -13,6 +16,51 @@ public class KeysFinder {
         this.keysGenerators = keysGenerators;
         listOfKeys = new ArrayList<>();
     }
+
+    public List<int[]> mTFindS(boolean isPair) throws InterruptedException, ExecutionException {
+        long keysNum = KeysNumFinder.keysNum(base,power);
+        ExecutorService exs = Executors.newCachedThreadPool();
+        ArrayList<Callable<List<int[]>>> tasks = new ArrayList<>();
+        for (Map.Entry<KeysGenerator, Boolean> kG : keysGenerators.entrySet()) {
+            if (kG.getValue() == isPair) {
+                tasks.add(() -> {
+                    return kG.getKey().getKeys();//System.out.println(listOfKeysTS);
+                });
+            }
+        }
+        List<Future<List<int[]>>> futures = exs.invokeAll(tasks);
+        for (Future<List<int[]>> future : futures) {
+            results.addAll(future.get());
+        }
+        exs.shutdown();
+        if (keysNum != results.size()) {
+            mTFindS(false);
+        }
+
+        return results;
+    }
+
+//    public void mTFind(boolean isPair) throws InterruptedException {
+//        ExecutorService exs = Executors.newCachedThreadPool();
+//
+//        for (Map.Entry<KeysGenerator, Boolean> kG : keysGenerators.entrySet()) {
+//            if (kG.getValue() == isPair) {
+//                exs.execute(() -> {
+//                    synchronized(listOfKeysTS) {
+//                        listOfKeysTS.add(kG.getKey().getKeys());
+//                    }
+//                });
+//            }
+//        }
+//        exs.shutdown();
+//        exs.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+////        if (KeysNumFinder.keysNum(base, power) != listOfKeys.size()) {
+////            mTFind(false);
+////        }
+//
+//        System.out.println(listOfKeys.size());
+//    }
+
 
     public void find(boolean isPair) {
         for (Map.Entry<KeysGenerator, Boolean> kG : keysGenerators.entrySet()) {
--- Dispatcher.java
+++ Dispatcher.java
@@ -1,14 +1,16 @@
 import Controllers.BasePairsController;
 import Controllers.KeysFinder;
+import Controllers.KeysNumFinder;
 import View.View;
 
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.concurrent.ExecutionException;
 
 public class Dispatcher {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException, ExecutionException {
         int base = 11;
-        int power = 3;
+        int power = 5;
 
         HashMap<Integer, Boolean> basePair = new HashMap<>(BasePairsController.findOne(base));
         HashMap<int[], Boolean> arrayPairs = new HashMap<>(BasePairsController.generatePairsArray(basePair, power));
@@ -18,7 +20,14 @@ public class Dispatcher {
 
         KeysFinder kF = new KeysFinder(base, power, BasePairsController.generateGenerators(base, power, arrayPairs));
 
-        kF.find(true);
+        long startTime = System.currentTimeMillis();
+        List<int[]> list = kF.mTFindS(true);
+        long endTime = System.currentTimeMillis();
+        if (KeysNumFinder.keysNum(base, power) == list.size()) {
+            System.out.println("yes " + list.size());
+        }
+        System.out.println("Total execution time: " + ((endTime / 1000)-(startTime / 1000)) + "s");
+
 
     }
 }
