52ccd4bfc308c6abb8b5271bd73b8afab03eca3d
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2022-05-28 22:44:14 +0300

    manual add all combination for p = 5..19 where keys exists.

---
+++ Controllers/KeysChecker.java
@@ -0,0 +1,22 @@
+package Controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeysChecker {
+    private HashMap<Integer, Boolean> basePairs;
+
+    public KeysChecker(HashMap<Integer, Boolean> basePairs) {
+        this.basePairs = basePairs;
+    }
+
+//    public void isKeysExist() {
+//        HashMap<Integer, Boolean> isExists = new HashMap<>();
+//        for (Map.Entry<Integer, Boolean> entry : basePairs.entrySet()) {
+//            for () {
+//                isExists.put(entry.getKey().probablyKeys.get(0)[0], kG.getKey().checkIsKeysExist());
+//            }
+//        }
+//        System.out.println(isExists);
+//    }
+}
--- Dispatcher.java
+++ Dispatcher.java
@@ -9,7 +9,7 @@ import java.util.concurrent.ExecutionException;
 public class Dispatcher {
     public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
         int base = 13;
-        int power = 7;
+        int power = 6;
 
         HashMap<Integer, Boolean> basePair = new HashMap<>(BasePairsController.findOne(base));
         HashMap<int[], Boolean> arrayPairs = new HashMap<>(BasePairsController.generatePairsArray(basePair, power));
@@ -17,19 +17,21 @@ public class Dispatcher {
         BasePairsController.findAll();
         View.show();
 
-        KeysFinder kF = new KeysFinder(base, power, BasePairsController.generateGenerators(base, power, arrayPairs));
-        kF.isKeysExist();
 
-//        new File("Logs/").mkdir();
-//        new File("Final/").mkdir();
-//        long startTime = System.currentTimeMillis();
-//        kF.mTFindF(true);
-//        long endTime = System.currentTimeMillis();
-//
-//
-//        FileController.concatFiles();
-//
-//        System.out.println("Total execution time: " + ((endTime / 1000)-(startTime / 1000)) + "s");
+
+        KeysFinder kF = new KeysFinder(base, power, BasePairsController.generateGenerators(base, power, arrayPairs));
+        //kF.isKeysExist();
+
+        new File("Logs/").mkdir();
+        new File("Final/").mkdir();
+        long startTime = System.currentTimeMillis();
+        kF.mTFindF(true);
+        long endTime = System.currentTimeMillis();
+
+
+        FileController.concatFiles();
+
+        System.out.println("Total execution time: " + ((endTime / 1000)-(startTime / 1000)) + "s");
 
 
     }
